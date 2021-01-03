c3ee78322c192ed26ad5092815d483ac738266ba
ewanh26 <73652324+ewanh26@users.noreply.github.com>
2021-01-03 20:53:59 +0000

    Update member_counter.js

--- counters/member_counter.js
+++ counters/member_counter.js
@@ -2,7 +2,7 @@ module.exports = async (client) => {
     const server = client.guilds.cache.get('786722960080633886');
 
     setInterval(() => {
-        const memberCount = server.memberCount;
+        const memberCount = server.members.cache.filter(member => !member.user.bot).size;
         const channel = server.channels.cache.get('795265617976492072');
         channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
         console.log('MEMBERCOUNT_UPDATE');
