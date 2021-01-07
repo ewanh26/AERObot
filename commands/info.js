641d311f05e8e2222e5fb821d91fc6b41c35b2c4
ewanh26 <73652324+ewanh26@users.noreply.github.com>
2021-01-07 16:44:02 +0000

    Update info.js, package.json

--- commands/info.js
+++ commands/info.js
@@ -3,6 +3,6 @@ module.exports = {
     usage: 'a!info',
     desc: 'Gives info about the bot.',
     execute(message, args) {
-        message.channel.send("Discord bot from AERO AI, made with JavaScript. Powered by Node.js runtime server and the Discord.js module");
+        message.channel.send("Discord bot from AERO AI, made with JavaScript. Powered by Node.js runtime and the Discord.js module. Runs on a Heroku server 24/7.");
     }
 }
