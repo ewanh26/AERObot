4585c0dc49ccafdebfaa73be8122329bb922d370
ewanh26 <73652324+ewanh26@users.noreply.github.com>
2021-01-07 16:31:46 +0000

    Update request.js

--- commands/request.js
+++ commands/request.js
@@ -5,11 +5,17 @@ module.exports = {
     execute(message, args, DISCORD, client) {
         const server = client.guilds.cache.get('786722960080633886');
         const requestChannel = server.channels.cache.get('796767132628942860');
+        const requestEmbed = DISCORD.MessageEmbed()
+        .setColor('#3377AA')
+        .setTitle('Request sent!')
+        .setDescription('The Admins will get back to you soon!')
+        .setImage('https://media1.tenor.com/images/49c81401a7a05013aeed8e8edb0875e0/tenor.gif?itemid=3561207');
 
         if (!args[1]) {
             message.channel.send('Please enter a request');
         } else {
-            requestChannel.send(`Request from ${message.author.toString()}: ${args.slice(0, args.length).join(' ')}`)
+            message.channel.send(requestEmbed);
+            requestChannel.send(`Request from ${message.author.toString()}: ${args.slice(0, args.length).join(' ')}`);
         }
     }
 }
